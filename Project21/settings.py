45a5f3eb4374a38a430e2aef9fb14b422d815c3e
Lakshminarayana <behera.lakshminarayana9@gmail.com>
2026-09-19 15:15:04 +0530

    Prepare for deployment

--- Project21/settings.py
+++ Project21/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-br%hhe!p3!8k*&t)j$%tyaoddzsd8u%949emf%#(4ccd#jzb7#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -124,3 +124,4 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
